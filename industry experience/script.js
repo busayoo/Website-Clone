bd6741990070edd7af67865f76c0429f0d2d257a
busayoo <103973492+busayoo@users.noreply.github.com>
2022-08-16 23:35:26 +0100

    Zuri-Website

--- industry experience/script.js
+++ industry experience/script.js
@@ -3,8 +3,21 @@ var btn = document.getElementById("modal_btn");
 btn.onclick = function () {
     modal.style.display = "block";
 }
-window.onclick = function (event) {
+
+windowey.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+}
+
+var boxmodal = document.getElementById("mymodal");
+var btn1 = document.getElementById("mybtn");
+btn1.onclick = function () {
+    boxmodal.style.display = "block";
+}
+
+window.onclick = function (event) {
+    if (event.target == boxmodal) {
+       boxmodal.style.display = "none";
     }
 }
